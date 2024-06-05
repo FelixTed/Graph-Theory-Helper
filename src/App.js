3fdb75560c7ca23bf5f82a9da3a0745b74fbbd6a
Felix Cadieux <cadieuxfelix@outlook.com>
2024-06-05 16:49:29 -0400

    display graph in box to right of screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CytoscapeComponent from 'react-cytoscapejs';
+
+const Network = () => {
+  const elements = [
+    { data: { id: 'a' } },
+    { data: { id: 'b' } },
+    {data: {id: 'c'}},
+    {data : {id:'ac',source:'a', target:'c'}},
+    { data: { id: 'ab', source: 'a', target: 'b' } }
+  ];
+
+  const layout = {
+    name: 'random',
+  
+  };
+
+  const style = [
+    {
+      selector: 'node',
+      style: {
+        'border-color': '#000000',
+        'border-width': 2, 
+        'background-color': '#FFFFFF',
+        'label': 'data(id)'
+      }
+    },
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#000000',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ];
+
+  return (
+    <div className='cytoscape-container '>
+    <CytoscapeComponent 
+      elements={elements} 
+      style={{ width: '600px', height: '600px' }} 
+      layout={layout} 
+      stylesheet={style} 
+    />
+    </div>
+  );
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Graph
-        </a>
-      </header>
+      <Network />
     </div>
   );
 }
